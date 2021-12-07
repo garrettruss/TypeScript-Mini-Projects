4113148ca3d689a4eae2440bbdaea2c668b27de9
garrettruss <garrettmruss@gmail.com>
2021-12-07 08:27:56 -0500

    arrays

---
+++ Arrays/Types/index.ts
@@ -0,0 +1,5 @@
+// Arrays:
+let bestNumbers: number[] = [7,77,4];
+let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+let bestBreakfasts: string[]= ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+let bestBooleans: boolean[] = [true, false];
