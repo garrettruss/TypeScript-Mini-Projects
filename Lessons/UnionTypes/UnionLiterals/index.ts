4158ecc461bd1b1dca100529c11c1462066ea1ce
garrettruss <garrettmruss@gmail.com>
2021-12-17 12:06:12 -0500

    unions

--- Lessons/UnionTypes/UnionLiterals/index.ts
+++ Lessons/UnionTypes/UnionLiterals/index.ts
@@ -10,7 +10,8 @@ function changeLight(color: Color) {
 }
 With the code above, we could ensure that wherever changeLight() is called, that it gets passed only allowed stoplight colors. If we tried to call changeLight('purple'), TypeScript would complain, since that is not a valid stoplight color.
 
-This technique allows us to write functions that are specific about the states they can handle, which helps us write code that’s less prone to errors.
+This technique allows us to write functions that are specific about the states they can handle, which helps us write code that’s less prone to errors. 
+
  */
 
 type Status = 'idle' | 'downloading' | 'complete';
